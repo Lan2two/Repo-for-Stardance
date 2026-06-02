0e3665941721b899acdc7d0fae2d4414f66ef242
Lan2two <lantotwo@email.com>
2026-06-01 23:02:03 -0400

    Offloaded the logic for moving to a velocity component

--- scripts/components/VelocityComponent.cs
+++ scripts/components/VelocityComponent.cs
@@ -3,5 +3,43 @@ using System;
 
 public partial class VelocityComponent : Node2D
 {
-    [Export] public float Speed = 300.0f;
+    [Export] public CharacterBody2D characterBody;
+    [Export] public float maxSpeed = 100.0f;
+    [Export] public float acceleration = 40.0f;
+
+    public Vector2 Velocity;
+
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public void AccelerateToVelocity(Vector2 velocity)
+    {
+        Velocity = Velocity.Lerp(velocity, acceleration * (float)GetProcessDeltaTime());
+    }
+    public void AccelerateToDirection(Vector2 direction)
+    {
+        Velocity = Velocity.Lerp(direction * maxSpeed, acceleration * (float)GetProcessDeltaTime());
+    }
+    public Vector2 GetMaxVelocity()
+    {
+        return Velocity * maxSpeed;
+    }
+    public void Decelerate()
+    {
+        Velocity = Velocity.Lerp(Vector2.Zero, acceleration * (float)GetProcessDeltaTime());
+    }
+    public void Move(Vector2 direction)
+    {
+        if (direction.X != 0)
+        {
+            this.GetPlayer().anim.FlipH = direction.X < 0;
+        }
+        characterBody.Velocity = Velocity;
+        characterBody.MoveAndSlide();
+    }
+
+
 }
--- scripts/player/Move.cs
+++ scripts/player/Move.cs
@@ -3,25 +3,20 @@ using System;
 
 public partial class Move : States
 {
-    [Export] public float Speed = 300.0f;
+    [Export] public VelocityComponent Velocity;
     public override void Enter()
     {
         this.GetPlayer().anim.Play("move");
     }
     public override void PhysicsUpdate(double delta)
     {
-        Vector2 velocity = this.GetPlayer().Velocity;
 
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
         Vector2 direction = Input.GetVector("left", "right", "up", "down");
-        //flip the sprite based on direction
-        if (direction.X != 0)
-        {
-            this.GetPlayer().anim.FlipH = direction.X < 0;
-        }
-        this.GetPlayer().Velocity = direction * Speed;
-        this.GetPlayer().MoveAndSlide();
+        Velocity.AccelerateToDirection(direction);
+        Velocity.Move(direction);
+
         if (direction == Vector2.Zero)
         {
             ChangeState("Idle");
