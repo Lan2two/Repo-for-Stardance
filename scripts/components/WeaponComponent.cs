72935cb90bb9defb79f44d4788f2492a20291455
Lan2two <lantotwo@email.com>
2026-06-05 00:32:20 -0400

    Making a melee weapon also background or some

---
+++ scripts/components/WeaponComponent.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+
+public partial class WeaponComponent : Node
+{
+    [Export] SpriteFrames spriteFrames;
+    [Export] public float swingCooldown;
+    [Export] public float hitboxDuration;
+    AnimatedSprite2D animatedSprite2D;
+    DamageComponent damageComponent;
+    private double timer = 0;
+    private double hitboxTimer = 0;
+    public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        damageComponent = GetNode<DamageComponent>("DamageComponent");
+        animatedSprite2D.SpriteFrames = spriteFrames;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        timer -= delta;
+        hitboxTimer -= delta;
+
+        if (hitboxTimer <= 0)
+        {
+            damageComponent.SetDeferred("monitoring", false);
+        }
+
+        if (Input.IsActionJustPressed("m1") && timer < 0)
+        {
+            timer = swingCooldown;
+            hitboxTimer = hitboxDuration;
+            animatedSprite2D.Play("swing");
+            damageComponent.SetDeferred("monitoring", true);
+        }
+    }
+
+
+
+}
